9fe44ada772329c8de4a6a875f6ffbadfbc38f05
Abdurabu Gubran <abumoaadg@hotmail.com>
2025-09-14 14:11:28 +0300

    update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -41,7 +41,8 @@ export const Badge = (props: Props) => {
   return (
     <>
       <Show when={props.footer?.showFooter === undefined || props.footer?.showFooter === null || props.footer?.showFooter === true}>
-        <span class="w-full text-center px-[10px] pt-[6px] pb-[10px] m-auto text-[13px]"
+        <span
+          class="w-full text-center px-[10px] pt-[6px] pb-[10px] m-auto text-[13px]"
           style={{
             color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor,
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
